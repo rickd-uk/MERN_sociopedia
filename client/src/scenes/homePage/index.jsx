947369a71ebd43ea9bd2eefe761e13aea18bc36d
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-12-23 21:49:53 +0900

    add: navbar

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,5 +1,13 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import Navbar from '../navbar'
+
 const HomePage = () => {
-	return <div>Homepage</div>
+	return (
+		<Box>
+			<Navbar />
+		</Box>
+	)
 }
 
 export default HomePage
